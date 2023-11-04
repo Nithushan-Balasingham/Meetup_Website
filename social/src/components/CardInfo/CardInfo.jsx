161ee6624e8d49e171aee448cf8bf9c109348bfd
Nithushan-Balasingham <nithushanbalasingham@gmail.com>
2023-11-04 20:05:34 +0530

    feat:Border

--- social/src/components/CardInfo/CardInfo.jsx
+++ social/src/components/CardInfo/CardInfo.jsx
@@ -15,38 +15,41 @@ const CardInfo = () => {
     <div>
       <div className="flex justify-center items-center p-4">
         <div
-          className="flex flex-col items-center justify-center shadow-xl rounded-xl w-80 bg-green-700"
+          className="flex flex-col items-center justify-center shadow-xl rounded-xl"
           key={cardDetails.id}
         >
           <div>
             <img
               src={cardDetails.eventImage}
               alt=""
-              className="w-72 h-56 rounded-xl p-2"
+              className="w-98 h-56 rounded-xl p-2 flex justify-start"
             />
           </div>
-          <div className="text-teal-400 font-bold">{cardDetails.eventName}</div>
-          <div className="text-white">{cardDetails.eventDescription}</div>
-          <div className="flex items-center justify-center">
-            <div className="m-2 text-white">{cardDetails.peopleGoing}</div>
+          <div className="text-teal-400 font-bold text-4xl">{cardDetails.eventName}</div>
+          <div className="text-white text-3xl">{cardDetails.eventDescription}</div>
+          <div className="flex items-center  flex-col justify-center text-3xl">
+            <div className="m-2 text-white font-mono">{cardDetails.peopleGoing}</div>
             <div
               className={
                 cardDetails.status === "Free"
-                  ? "text-teal-400 font-bold"
-                  : "text-rose-400 font-bold"
+                  ? "text-teal-400 font-bold font-mono"
+                  : "text-rose-400 font-bold font-mono"
               }
             >
               {cardDetails.status}
             </div>
           </div>
+          <div className="text-teal-700 font-bold text-xl">
+            Images
+          </div>
+          <div className="flex w-full justify-center items-center shadow-xl shadow-black/40 p-8">
+            <img src="https://addicted2success.com/wp-content/uploads/2018/06/8-Reasons-You-Should-Join-a-Meetup-Group-Today.jpg" alt="" className='w-72 h-56 rounded-xl p-2 hover:scale-110 transition duration-300'/>
+            <img src="https://blog.euvou.events/wp-content/uploads/2021/06/meetup.png" alt="" className='w-72 h-56 rounded-xl p-2 hover:scale-110 transition duration-300'/>
+            <img src="https://miro.medium.com/v2/resize:fit:1400/1*xv_OrvHjTRjb09SrWHqT-A.jpeg" alt="" className='w-72 h-56 rounded-xl p-2 hover:scale-110 transition duration-300'/>
+            <img src="https://www.meetup.com/blog/wp-content/uploads/2022/01/conversation-over-coffee_how-to-make-conversation-1120x630.jpg" alt="" className='w-72 h-56 rounded-xl p-2 hover:scale-110 transition duration-300'/>
+        </div>
         </div>
       </div>
-          <div className="flex w-full justify-center items-center">
-        <img src="https://addicted2success.com/wp-content/uploads/2018/06/8-Reasons-You-Should-Join-a-Meetup-Group-Today.jpg" alt="" className='w-72 h-56 rounded-xl p-2'/>
-        <img src="https://blog.euvou.events/wp-content/uploads/2021/06/meetup.png" alt="" className='w-72 h-56 rounded-xl p-2'/>
-        <img src="https://miro.medium.com/v2/resize:fit:1400/1*xv_OrvHjTRjb09SrWHqT-A.jpeg" alt="" className='w-72 h-56 rounded-xl p-2'/>
-        <img src="https://www.meetup.com/blog/wp-content/uploads/2022/01/conversation-over-coffee_how-to-make-conversation-1120x630.jpg" alt="" className='w-72 h-56 rounded-xl p-2'/>
-        </div>
     </div>
   );
 };
